c362783f1ffeaea185381e1a270f0dd9586415b6
Johntherocker <cederdahl@gmail.com>
2025-06-28 16:57:35 +0200

    Update app-copy.py

--- app-copy.py
+++ app-copy.py
@@ -18,8 +18,8 @@ def download_file(url, local_path):
             f.write(r.content)
 
 def download_faiss_index():
-    faiss_url = "https://www.dropbox.com/scl/fi/y5kw2ut5orxhww7a6pydp/index.faiss?rlkey=igd39gip391qvbutmil5hw3u8&st=3wdtafag&dl=1"
-    pkl_url = "https://www.dropbox.com/scl/fi/kly0btqxm9tfd02wnxzds/index.pkl?rlkey=3tg1bdyemk4wx74htbjzct21a&st=u89nmt2w&dl=1"
+    faiss_url = "https://www.dropbox.com/scl/fi/kly0btqxm9tfd02wnxzds/index.pkl?rlkey=3tg1bdyemk4wx74htbjzct21a&st=ug8w4ebt&dl=1"
+    pkl_url = "https://www.dropbox.com/scl/fi/ub3eva6qo8c7cht5h53c1/index2.pkl?rlkey=pe6ka420v3vul0i89vuprub59&st=144q8kbi&dl=1"
 
     index_faiss_path = Path("/tmp/index.faiss")
     index_pkl_path = Path("/tmp/index.pkl")
