2dcbcbb22225a7b6d4ebf52631b4e4696c66c4e5
Johntherocker <cederdahl@gmail.com>
2025-06-28 17:03:11 +0200

    Update app-copy.py

--- app-copy.py
+++ app-copy.py
@@ -21,8 +21,8 @@ def download_faiss_index():
     faiss_url = "https://www.dropbox.com/scl/fi/kly0btqxm9tfd02wnxzds/index.pkl?rlkey=3tg1bdyemk4wx74htbjzct21a&st=ug8w4ebt&dl=1"
     pkl_url = "https://www.dropbox.com/scl/fi/ub3eva6qo8c7cht5h53c1/index2.pkl?rlkey=pe6ka420v3vul0i89vuprub59&st=144q8kbi&dl=1"
 
-    index_faiss_path = Path("/tmp/index.faiss")
-    index_pkl_path = Path("/tmp/index.pkl")
+    index_faiss_path = Path("/tmp/index2.faiss")
+    index_pkl_path = Path("/tmp/index2.pkl")
 
     if not index_faiss_path.exists():
         r = requests.get(faiss_url)
